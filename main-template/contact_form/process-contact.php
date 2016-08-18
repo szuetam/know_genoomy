ac7bd6132642127a476b1ce11c9dcccd5b34ec0d
jvn <jiivan@o2.pl>
2016-08-18 14:41:46 +0200

    contact form poprawić trzeba tak, aby wysyłał maila na adres contact_form@genoomy.com

--- main-template/contact_form/process-contact.php
+++ main-template/contact_form/process-contact.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_POST['action'])) { // Checking for submit form
 	
-	$my_email = 'support@example.com'; // Change with your email address
+	$my_email = 'contact_form@genoomy.com'; // Change with your email address
 	
 	if ($_POST['action'] == 'add') {
 		$name		= trim(strip_tags(addslashes($_POST['name'])));
